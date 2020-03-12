7c60126fea4ae01855d286fa04dd6f95cb772fe6
shin3250 <git@shin3250.net>
2020-03-12 13:15:43 +0900

    First commit

---
+++ disable-srt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace disable_srt {
+    class Program {
+        static void Main(string[] args) {
+            var userProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            var SwReporter = userProfile + @"\AppData\Local\Google\Chrome\User Data\SwReporter";
+
+            foreach(var filePath in Directory.GetFiles(SwReporter, "software_reporter_tool.exe", SearchOption.AllDirectories)) {
+                try {
+                    var fileSecurity = File.GetAccessControl(filePath);
+                    fileSecurity.SetAccessRuleProtection(true, false);
+                    File.SetAccessControl(filePath, fileSecurity);
+
+                    if(args.Length == 0) SystemSounds.Beep.Play();
+                } catch {
+                    try { if(args.Length == 0) SystemSounds.Hand.Play(); } catch { }
+                }
+            }
+
+        }
+    }
+}
---
+++ disable-srt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("disable-srt")]
+[assembly: AssemblyDescription("Disable software_reporter_tool.exe")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("disable-srt")]
+[assembly: AssemblyCopyright("Copyright © 2020 shin3250")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("0d083133-78ad-4419-b01a-46ab1744d8ac")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
